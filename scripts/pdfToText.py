424d59141e4324ac381e53229b52f0b95430278e
m257074 <m257074@usna.edu>
2025-01-09 22:29:37 -0500

    PDF Reader : Ziman

---
+++ scripts/pdfToText.py
@@ -0,0 +1,28 @@
+import PyPDF2
+import sys
+
+def pdf_to_text(pdf_path, output_txt):
+    # Open the PDF file in read-binary mode
+    with open(pdf_path, 'rb') as pdf_file:
+        # Create a PdfReader object instead of PdfFileReader
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+        # Initialize an empty string to store the text
+        text = ''
+
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
+
+    # Write the extracted text to a text file
+    with open(output_txt, 'w', encoding='utf-8') as txt_file:
+        txt_file.write(text)
+
+if __name__ == "__main__":
+    pdf_path = sys.argv[0]
+
+    output_txt = sys.argv[0].split(".")[0] + ".txt"
+
+    pdf_to_text(pdf_path, output_txt)
+
+    print("PDF converted to text successfully!")
