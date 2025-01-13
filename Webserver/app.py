477f7bdd3cf89ede529b52166779358cf19b4afd
m253300 <m253300@usna.edu>
2025-01-12 19:48:33 -0500

    create webserver

---
+++ Webserver/app.py
@@ -0,0 +1,18 @@
+# navigate to the environment directory and do ".\Scripts\activate"
+# navigate to the directory with the flask file and do "python app.py"
+
+from flask import Flask, render_template
+ 
+app = Flask(__name__)
+ 
+ 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+ 
+if __name__ == "__main__":
+   app.run()
