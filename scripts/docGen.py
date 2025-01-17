1d4604abdd904ac0fd6f098ae97303840d159253
sideoffryes <m251854@usna.edu>
2025-01-16 20:11:05 -0500

    added text streaming

--- scripts/docGen.py
+++ scripts/docGen.py
@@ -1,19 +1,23 @@
-from transformers import pipeline, logging
-import torch
+from transformers import pipeline, logging, AutoTokenizer, TextStreamer
 import os
 import time
 from datetime import datetime
 import argparse
+import torch
+
+# export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 
 # parse arguments
 parser = argparse.ArgumentParser(description="Generates military documents via an LLM based on user input")
 parser.add_argument("-t", "--max-tokens", type=int, help="Specify the max number of tokens when generating the document, default is 500", default=500)
 args = parser.parse_args()
 
-# torch.set_num_threads(32)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = "meta-llama/Llama-3.1-8B-Instruct"
 
-generator = pipeline("text-generation", model="meta-llama/Llama-3.2-1B-Instruct", device=device)
+# generator = pipeline("text-generation", model="meta-llama/Llama-3.2-11B-Vision-Instruct", device=device, device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model)
+streamer = TextStreamer(tokenizer, skip_prompt=True)
+generator = pipeline("text-generation", model, tokenizer=tokenizer, device_map="auto", torch_dtype=torch.float16)
 logging.set_verbosity_error()
 
 def clean_output(text: str) -> str:
@@ -60,12 +64,9 @@ format = "Give your answer in naval message format based on the previous example
 prompt = role + examples + user_query + format
 
 t_start = time.time()
-response = generator(prompt, max_new_tokens=args.max_tokens)[0]['generated_text'][len(prompt):]
+response = generator(prompt, max_new_tokens=args.max_tokens, streamer=streamer)[0]['generated_text'][len(prompt):]
 t_stop = time.time()
 
-# response = clean_output(response)
-
-print(response)
 print(f"Generation time: {t_stop - t_start}")
 
 # save response to file
