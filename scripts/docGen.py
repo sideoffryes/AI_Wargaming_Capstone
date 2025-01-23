774689af6a81a6d11b3706d7645f5793e18d6f84
sideoffryes <m251854@usna.edu>
2025-01-23 15:12:01 -0500

    modified gen function api to use numbers

--- scripts/docGen.py
+++ scripts/docGen.py
@@ -16,18 +16,20 @@ args = parser.parse_args()
 
 # Set LLM instructions
 ROLE = "Role: You work for the United States Department of the Navy, and you specialize in writing official military documents using military formatting.\n"
-FORMAT = "Give your answer in naval message format based on the previous examples."
+SYSTEM = "Give your answer in naval message format based on the previous examples."
 
-def gen(model_name: str, type: str, prompt: str, save: bool = True) -> str:
+def gen(model_num: int, type_num: int, prompt: str, save: bool = True) -> str:
     # create model objects
+    model_name = select_model(model_num)
+    doc_type = select_doc(type_num)
     model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", torch_dtype=torch.float16)
     tokenizer = AutoTokenizer.from_pretrained(model_name, padding_size="left")
     tokenizer.pad_token = tokenizer.eos_token
     streamer = TextStreamer(tokenizer, skip_prompt=True)
     
     # set up prompt info
-    examples = load_examples(type)
-    prompt = ROLE + examples + prompt + FORMAT
+    examples = load_examples(doc_type)
+    prompt = ROLE + examples + prompt + SYSTEM
     
     # set device based on gpu availability
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -35,7 +37,9 @@ def gen(model_name: str, type: str, prompt: str, save: bool = True) -> str:
     
     # generate response
     t_start = time.time()
+    # TODO: Is the streamer actually working as intended, too much overhead? How does this relate to batch_decode?
     generated_ids = model.generate(**model_inputs, do_sample=True, max_new_tokens=args.max_tokens, streamer=streamer)
+    # TODO: Do I need to slice off the prompt?
     response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0][len(prompt):]
     t_stop = time.time()
     print(f"Generation time: {t_stop - t_start} sec / {(t_stop - t_start) / 60} min")
@@ -74,12 +78,10 @@ def save_response(response: str, prompt: str, model_name: str, model: str):
         file.write(f"---------- PROMPT ----------\n{prompt}\n\n")
         file.write(f"---------- MODEL ----------\n{model_name}\n{model}")
 
-if __name__ == "__main__":
-    # model to select model you want to load
-    select = int(input("Select the Llama model you would like to run\n1) Llama 3.2 1B Instruct\n2) Llama 3.2 3B Instruct\n3) Llama 3.1 8B Instruct\n4) Llama 3.3 70B Instruct\n> "))
+def select_model(num: int) -> str:
     model = ""
-
-    match select:
+    
+    match num:
         case 1:
             model = "meta-llama/Llama-3.2-1B-Instruct"
         case 2:
@@ -88,12 +90,13 @@ if __name__ == "__main__":
             model = "meta-llama/Llama-3.1-8B-Instruct"
         case 4:
             model = "meta-llama/Llama-3.3-70B-Instruct"
-            
-    # get document type from user
-    doc = int(input("Select document to generate\n1) Naval Message (NAVADMIN)\n2) USMC Message (MARADMIN)\n3) USMC OpOrd\n> "))
-    type = ""
 
-    match doc:
+    return model
+
+def select_doc(num: int) -> str:
+    type = ""
+    
+    match num:
         case 1:
             type = "NAVADMIN"
         case 2:
@@ -102,9 +105,18 @@ if __name__ == "__main__":
             type = "OpOrd"
         case _:
             type = "NAVADMIN"
-    
+
+    return type
+
+if __name__ == "__main__":
+    # model to select model you want to load
+    select = int(input("Select the Llama model you would like to run\n1) Llama 3.2 1B Instruct\n2) Llama 3.2 3B Instruct\n3) Llama 3.1 8B Instruct\n4) Llama 3.3 70B Instruct\n> "))
+
+    # get document type from user
+    doc = int(input("Select document to generate\n1) Naval Message (NAVADMIN)\n2) USMC Message (MARADMIN)\n3) USMC OpOrd\n> "))
+
     user_query = input("Input> ")
     user_query = "Request: " + user_query
 
     # call document generator function
-    doc = gen(model, type, user_query)
+    doc = gen(select, doc, user_query)
