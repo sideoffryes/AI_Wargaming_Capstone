b103f3d63f53c1cb86bb66bb3c25d77681f9d9c7
sideoffryes <m251854@usna.edu>
2025-01-30 10:36:57 -0500

    began integrating sphinx documentation

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. AI Wargaming documentation master file, created by
+   sphinx-quickstart on Thu Jan 30 10:32:27 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to AI Wargaming's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'AI Wargaming'
+copyright = '2025, Peter Asjes, Henry Frye, Caleb Koutrakos, Will Robinson, and Bobby Ziman'
+author = 'Peter Asjes, Henry Frye, Caleb Koutrakos, Will Robinson, and Bobby Ziman'
+release = '0.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
