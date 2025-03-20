e8a96bdc33aff221aeb9f00be21b0278aa1e4520
sideoffryes <m251854@usna.edu>
2025-03-20 09:36:04 -0400

    spelling

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ read -p "Would like to perform the GPU or CPU setup? " opt
 if [[ "$opt" == "GPU" ]]; then
     pip install -r requirements_gpu.txt
 elif [[ "$opt" == "CPU" ]]; then
-    pip install -r requiremments_cpu.txt
+    pip install -r requirements_cpu.txt
 else
     echo "You did not select a valid option! Only enter GPU or CPU!"
     exit
